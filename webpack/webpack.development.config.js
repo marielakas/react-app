dcc80a43a58a55a9a31f32b879c6da07983dd6b9
Mariela Kasovska <marielakasovska@Marielas-MacBook-Pro.local>
2018-06-14 17:00:07 +0300

    Add webpack entry and output config

--- webpack/webpack.development.config.js
+++ webpack/webpack.development.config.js
@@ -1,1 +1,23 @@
-module.exports = {};
+const path = require('path');
+
+const srcPath = path.join(__dirname, '..', 'src');
+const entryPath =  path.join(srcPath, 'bootstrap.js');
+const outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    mode: 'development',
+    context: srcPath,
+    entry: {
+        app: entryPath,
+        vendor: ['react']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: outputPath
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+};
