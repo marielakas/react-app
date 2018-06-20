e066f1f37378255f75ce82dff55189c2e6e413e6
Mariela Kasovska <mariela.kasovska@sumup.com>
2018-06-20 13:46:04 +0300

    Add components folder

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
 import store from "./store";
 
 render(
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { hot } from "react-hot-loader";
-import styles from "./styles.css";
 
 const Test = () => {
   return <div>TEST</div>;
@@ -10,7 +9,6 @@ const Test = () => {
 const App = () => {
   return (
     <div id="layout">
-      <header className={styles.test}>App header</header>
       <Switch>
         <Route exact path="/test" component={Test} />
       </Switch>
